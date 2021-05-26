7ffb6501fdfe48bdaa706956f0d57a6a151c401c
VictorDizne <victoralbertocostadiniz@gmail.com>
2021-05-26 16:33:02 -0300

    Exercicios do dia feitos

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,8 @@
+// document.getElementById("page-title").innerText = "Pulp Fiction";
+// document.getElementById("subtitle").innerText = "renatin";
+// document.getElementsByClassName("paragraph")[0].style.backgroundColor = "pink";
+// document.getElementsByTagName("h4")[0].style.color = 'blue';
+// let textoBranco = document.querySelectorAll(".emergency-tasks h3");
+// for (i = 0; i < textoBranco.length; i += 1) {
+//   textoBranco[i].style.color = "white";
+// }
